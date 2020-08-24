36535079cf4740b12ed6a043c1b01bd5a5fc4e0d
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-08-24 15:56:25 -0400

    Two number sum.

--- Arrays/two-number-sum.js
+++ Arrays/two-number-sum.js
@@ -1,23 +1,39 @@
-const array = [3, 5, -4, 8, 11, 1, -1, 6];
-const targetSum = 10;
 
-function twoNumberSum(array, targetSum) {
-	// Write your code here.
+//   Write a function that takes in a non-empty array of distinct integers and an
+//   integer representing a target sum. If any two numbers in the input array sum
+//   up to the target sum, the function should return them in an array, in any
+//   order. If no two numbers sum up to the target sum, the function should return
+//   an empty array.
+
+
+// Note that the target sum has to be obtained by summing two different integers
+// in the array; you can't add a single integer to itself in order to obtain the
+// target sum.
+
+
+// You can assume that there will be at most one pair of numbers summing up to
+// the target sum.
+
+const twoNumberSum = (array, targetSum) => {
 	let answer = [];
-	  
+	const hash = {};
+	
+	//Store in hash
+	array.forEach((item) => hash[item] = item);
+
+	//Loop through and check hash for number needed
 	for (let i = 0; i < array.length; i++) {
-		const outer = array[i]
-		const sumValue = array.find((inner) => outer !== inner && 
-											   outer + inner === targetSum )
+		const currentNumber= array[i];
+		const numberNeeded = targetSum - currentNumber;
 		
-		if (sumValue) {
-			answer.push(outer);
-			answer.push(sumValue);
+		if (hash[numberNeeded] !== undefined && hash[numberNeeded] !== currentNumber) {
+			answer.push(currentNumber);
+			answer.push(numberNeeded);
+
 			return answer;
 		}
-	};
-	  
+	}
+	
 	return answer;
 }
-
-console.log(twoNumberSum(array, targetSum), targetSum)
+  
