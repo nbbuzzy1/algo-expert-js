23e2553857cace3a32dadaff1a810bf1ff34ff2a
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-09-06 19:15:42 -0400

    Start move element to end.

---
+++ Arrays/move-element-to-end.js
@@ -0,0 +1,32 @@
+// You're given an array of integers and an integer. Write a function that moves
+// all instances of that integer in the array to the end of the array and returns
+// the array.
+
+// The function should perform this in place (i.e., it should mutate the input
+// array) and doesn't need to maintain the order of the other integers.
+
+// Sample Input
+// [2, 1, 2, 2, 2, 3, 4, 2]
+ 
+// 2
+
+// Sample Output
+// [1, 3, 4, 2, 2, 2, 2, 2] // the numbers 1, 3, and 4 could be ordered differently
+
+function moveElementToEnd(array, toMove) {
+	// Write your code here.
+	let moveToEndArray = [];
+	
+	array.forEach((num) => {
+		if (num === toMove) {
+			moveToEndArray.push(num);
+		} else {
+			moveToEndArray.unshift(num);
+		}
+	});
+	
+	return moveToEndArray;
+  }
+  
+  // Do not edit the line below.
+  exports.moveElementToEnd = moveElementToEnd;
