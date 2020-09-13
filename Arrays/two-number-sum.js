fa0b601c937769928f776efa7ab7991ec8df065a
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-09-13 17:26:12 -0400

    Some clean up of files.

--- Arrays/two-number-sum.js
+++ Arrays/two-number-sum.js
@@ -1,4 +1,3 @@
-
 //   Write a function that takes in a non-empty array of distinct integers and an
 //   integer representing a target sum. If any two numbers in the input array sum
 //   up to the target sum, the function should return them in an array, in any
--- DynamicProgramming/num-ways-to-make-change.js
+++ DynamicProgramming/num-ways-to-make-change.js
@@ -1,4 +1,3 @@
-
 //   Given an array of distinct positive integers representing coin denominations and a
 //   single non-negative integer representing a target amount of
 //   money, write a function that returns the number of ways to make change for
