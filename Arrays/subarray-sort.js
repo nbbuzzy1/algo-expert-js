3af05fcab50a77193f7e782664356c34cf68e097
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-11-14 10:24:49 -0500

    Subarray sort.

--- Arrays/subarray-sort.js
+++ Arrays/subarray-sort.js
@@ -11,3 +11,41 @@
 
 // Sample Output
 // [3, 9]
+
+function subarraySort(array) {
+	// Write your code here.
+	let minOutOfOrder = Infinity;
+	let maxOutOfOrder = -Infinity;
+
+	for (let i = 0; i < array.length; i++) {
+		const current = array[i];
+		if (isOutOfOrder(i, current, array)) {
+			minOutOfOrder = Math.min(minOutOfOrder, current);
+			maxOutOfOrder = Math.max(maxOutOfOrder, current);
+		}
+	}
+
+	if (minOutOfOrder === Infinity) return [-1, -1]; //for sorted arrays
+
+	let left = 0;
+	let right = array.length - 1;
+	while (minOutOfOrder >= array[left]) {
+		left++;
+	}
+
+	while (maxOutOfOrder <= array[right]) {
+		right--;
+	}
+
+	return [left, right];
+}
+
+function isOutOfOrder(i, num, array) {
+	if (i === 0) {
+		return num > array[i + 1];
+	} else if (i === array.length - 1) {
+		return num < array[i - 1];
+	} else {
+		return num > array[i + 1] || num < array[i - 1];
+	}
+}
