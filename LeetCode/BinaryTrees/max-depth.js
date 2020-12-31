f938b0455fd7490983d077f2af8d382c70ed74ec
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-12-31 14:08:47 -0500

    Iterative solution for binary tree problem.

--- LeetCode/BinaryTrees/max-depth.js
+++ LeetCode/BinaryTrees/max-depth.js
@@ -20,8 +20,31 @@
 // Input: root = [0]
 // Output: 1
 
-const maxDepth = (root, depth = 0) => {
-	if (!root) return depth;
+// const maxDepth = (root, depth = 0) => {
+// 	if (!root) return depth;
 
-	return Math.max(maxDepth(root.left, depth + 1), maxDepth(root.right, depth + 1));
-};
+// 	return Math.max(maxDepth(root.left, depth + 1), maxDepth(root.right, depth + 1));
+// };
+
+//Iterative Solution
+
+const maxDepth = (root) => {
+	if (!root) return 0;
+
+	const queue = [{ node: root, depth: 1 }];
+	let maxDepth = 1;
+
+	while (queue.length) {
+		const { node, depth } = queue.shift();
+		maxDepth = Math.max(maxDepth, depth);
+
+		if (node.left) addNodeToQueue(queue, node.left, depth + 1);
+		if (node.right) addNodeToQueue(queue, node.right, depth + 1);
+	}
+
+	return maxDepth;
+}
+
+const addNodeToQueue = (queue, node, depth) => {
+	queue.push({ node, depth });
+}
